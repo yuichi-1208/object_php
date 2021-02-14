343d54dc3c3a2b358803eea887db9d5d92241476
yuichi oyama <yuichi19991208@gmail.com>
2021-02-14 23:47:30 +0900

    コンストラクタ

--- main.php
+++ main.php
@@ -21,6 +21,17 @@ class Post
   public $text;
   public $likes;
 
+  // public function __construct($textFromNew, $likesFromNew)
+  // {
+  //   $this->text = $textFromNew;
+  //   $this->likes = $likesFromNew;
+  // }
+  public function __construct($text, $likes)
+  {
+    $this->text = $text;
+    $this->likes = $likes;
+  }
+
   // メソッド
   public function show()
   {
@@ -31,13 +42,13 @@ class Post
 $posts = [];
 
 // インスタンス
-$posts[0] = new Post();
-$posts[0]->text = 'hello';
-$posts[0]->likes = 0;
+$posts[0] = new Post('hello', 0);
+// $posts[0]->text = 'hello';
+// $posts[0]->likes = 0;
 
-$posts[1] = new Post();
-$posts[1]->text = 'hello again';
-$posts[1]->likes = 0;
+$posts[1] = new Post('hello again', 0);
+// $posts[1]->text = 'hello again';
+// $posts[1]->likes = 0;
 
 $posts[0]->show();
 $posts[1]->show();
