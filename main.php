949d3df085f3664605fa924bdebc280f273b4902
yuichi oyama <yuichi19991208@gmail.com>
2021-02-14 23:50:29 +0900

    の初期化処理をクラス内に移した

--- main.php
+++ main.php
@@ -19,17 +19,16 @@ class Post
 {
   // プロパティ
   public $text;
-  public $likes;
+  public $likes = 0;
 
   // public function __construct($textFromNew, $likesFromNew)
   // {
   //   $this->text = $textFromNew;
   //   $this->likes = $likesFromNew;
   // }
-  public function __construct($text, $likes)
+  public function __construct($text)
   {
     $this->text = $text;
-    $this->likes = $likes;
   }
 
   // メソッド
@@ -42,11 +41,11 @@ class Post
 $posts = [];
 
 // インスタンス
-$posts[0] = new Post('hello', 0);
+$posts[0] = new Post('hello');
 // $posts[0]->text = 'hello';
 // $posts[0]->likes = 0;
 
-$posts[1] = new Post('hello again', 0);
+$posts[1] = new Post('hello again');
 // $posts[1]->text = 'hello again';
 // $posts[1]->likes = 0;
 
