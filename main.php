790b07bdef9fb41d5f54ffe7a9a5f31f96ad2fb7
yuichi oyama <yuichi19991208@gmail.com>
2021-02-14 23:39:40 +0900

    クラスからインスタンスの作成

--- main.php
+++ main.php
@@ -1,15 +1,43 @@
 <?php
 
-$posts = [];
-$posts[0] = ['text' => 'hello', 'likes' => 0];
-$posts[1] = ['text' => 'hello again', 'likes' => 0];
+// $posts = [];
+// $posts[0] = ['text' => 'hello', 'likes' => 0];
+// $posts[1] = ['text' => 'hello again', 'likes' => 0];
 
-// print_r($posts);
+// // print_r($posts);
 
-function show($post)
+// function show($post)
+// {
+//   printf('%s (%d)' . PHP_EOL, $post['text'], $post['likes']);
+// }
+
+// show($posts[0]);
+// show($posts[1]);
+
+// クラス
+class Post
 {
-  printf('%s (%d)' . PHP_EOL, $post['text'], $post['likes']);
+  // プロパティ
+  public $text;
+  public $likes;
+
+  // メソッド
+  public function show()
+  {
+    printf('%s (%d)' . PHP_EOL, $this->text, $this->likes);
+  }
 }
 
-show($posts[0]);
-show($posts[1]);
+$posts = [];
+
+// インスタンス
+$posts[0] = new Post();
+$posts[0]->text = 'hello';
+$posts[0]->likes = 0;
+
+$posts[1] = new Post();
+$posts[1]->text = 'hello again';
+$posts[1]->likes = 0;
+
+$posts[0]->show();
+$posts[1]->show();
